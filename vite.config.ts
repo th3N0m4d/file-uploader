d0c06616de64ac6453e23adf3da69de8e2c4f8be
th3N0m4d <edielton.estrela.dantas@sap.com>
2025-10-12 12:43:09 +0200

    feat: add server proxy configuration for API requests

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,16 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target:
+          "https://hw8hqfytqi.execute-api.eu-central-1.amazonaws.com/prod",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",
