08ba14b6016b75d23dd42d9a77e5ed99ff8422ea
th3N0m4d <edielton.estrela.dantas@sap.com>
2025-10-16 14:57:28 +0200

    refactor: remove onRemove prop and associated icon from FileItem component

--- src/components/FileItem.tsx
+++ src/components/FileItem.tsx
@@ -1,45 +1,17 @@
 import type { UploadedFile } from "../hooks/useFileUpload";
+import { getFileIcon } from "../helpers";
 
 interface Props {
   file: UploadedFile;
-  onRemove: (fileId: string) => void;
 }
 
-const getFileIcon = (fileName: string): string => {
-  const extension = fileName.split(".").pop()?.toLowerCase();
-
-  switch (extension) {
-    case "pdf":
-      return "far fa-file-pdf";
-    case "txt":
-      return "far fa-file-alt";
-    case "doc":
-    case "docx":
-      return "far fa-file-word";
-    case "jpg":
-    case "jpeg":
-    case "png":
-    case "gif":
-    case "bmp":
-    case "webp":
-      return "far fa-file-image";
-    default:
-      return "far fa-file";
-  }
-};
-
-export const FileItem = ({ file, onRemove }: Props) => {
+export const FileItem = ({ file }: Props) => {
   return (
     <div key={file.id} className="uploaded">
       <i className={getFileIcon(file.name)}></i>
       <div className="file">
         <div className="file__name">
           <p>{file.name}</p>
-          <i
-            className="fas fa-times"
-            onClick={() => onRemove(file.id)}
-            role="button"
-          ></i>
         </div>
         <div className="progress">
           <div
