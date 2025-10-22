3beecdd2cb09f261cf0c859c6788c5e7a75410ce
th3N0m4d <edielton.estrela.dantas@sap.com>
2025-10-22 07:08:47 +0200

    feat: wrap App component with BrowserRouter for routing support

--- src/App.tsx
+++ src/App.tsx
@@ -11,15 +11,13 @@ import { UploadPage, FileListPage } from "./pages";
 
 function App() {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Navigate to="/upload" replace />} />
-          <Route path="/upload" element={<UploadPage />} />
-          <Route path="/files" element={<FileListPage />} />
-        </Routes>
-      </Layout>
-    </Router>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Navigate to="/upload" replace />} />
+        <Route path="/upload" element={<UploadPage />} />
+        <Route path="/files" element={<FileListPage />} />
+      </Routes>
+    </Layout>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
